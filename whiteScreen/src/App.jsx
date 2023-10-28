39bd133f6e6e6dc3b4bddffe91e131037ddfa362
sujith <sujisurendran10101@gmail.com>
2023-10-28 17:54:11 +0530

    basic routes

--- whiteScreen/src/App.jsx
+++ whiteScreen/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-export default function App () {
-  return(
 
-    <h1 className='text-red-700'>App</h1>
-  ) 
-}
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/signin" element={<SignIn />} />
+         <Route path="/signup" element={<SignUp />} />
+         <Route path="/profile" element={<Profile />} />
+         <Route path="/about" element={<About />} />
+       </Routes>
+    </BrowserRouter>
+  )  
+}  
+
+export default App
 
---
+++ whiteScreen/src/pages/SignUp.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div className='text-red-900'>SignUp</div>
+  )
+}
+export default SignUp
